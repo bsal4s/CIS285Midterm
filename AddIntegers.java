91bbe13a64a77f01224dbc2bead1c7ae011dd39b
Batoul <bsalmass@umich.edu>
2021-10-28 16:58:40 -0400

    final change for jenkins

--- AddIntegers.java
+++ AddIntegers.java
@@ -1,7 +1,7 @@
 public class AddIntegers {
   public static void main(String[] args){
-    int first = 20;
-    int second = 45;
+    int first = 35;
+    int second = 10;
     System.out.println("Enter two numbers: " + first + " " + second);
     int sum = first + second;
     System.out.println("The sum is: " + sum);
